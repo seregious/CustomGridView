4b9a04480335eef003b7fce9417a28c032033211
seregious <seregious@gmail.com>
2022-04-30 21:10:48 +0300

    Initial Commit

---
+++ CustomGridView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomGridView
+//
+//  Created by Сергей Иванчихин on 30.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomGridView/CustomGridViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomGridViewApp.swift
+//  CustomGridView
+//
+//  Created by Сергей Иванчихин on 30.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomGridViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
